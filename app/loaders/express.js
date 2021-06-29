13fbcb71446127ed7d2761438e78391e0eb901f1
Harrison Howard <harrison.howard00707@gmail.com>
2021-06-29 13:47:10 +1000

    Added helmet and mongo-sanitize security

--- app/loaders/express.js
+++ app/loaders/express.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const helmet = require("helmet");
 const morgan = require("morgan");
 
 module.exports = app => {
     app.use(cors());
+    app.use(helmet());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser());
