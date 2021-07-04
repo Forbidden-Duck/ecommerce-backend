32ff4ee0bb07fa886c9e3e568c8fee014e085367
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-04 14:44:17 +1000

    Improved tests (Instead of a big chunk of code for user checking, each route gets its own set of users)

--- test/index.test.js
+++ test/index.test.js
@@ -47,26 +47,33 @@ let testApp; // So we can close the connection
  * @type {import("supertest").SuperAgentTest}
  */
 let request; // Persist cookies
-/**
- * User created
- * @type {import("../app/db/schemas/users")}
- */
-let user;
-/**
- * Used for /api routes
- * @type {string}
- */
-let jwttoken;
 
-// Used to check a user
-const checkData = { // Password is either encrypted or removed
-    email: "SomeWeirdEmail@email.com",
-    firstname: "Super",
-    lastname: "Man"
+/**
+ * Create a new user
+ * @param {import("../app/db/schemas/users")} userObj 
+ * @returns
+ */
+const createUser = async userObj => {
+    const res = await supertest(testApp.app)
+        .post("/auth/register")
+        .set("Accept", "application/json")
+        .send(userObj);
+    return res;
 };
-// Used to authenticate a user
-const sendData = Object.assign({}, checkData);
-sendData.password = "password123";
+
+/**
+ * Login a user
+ * @param {supertest.SuperTest<supertest.Test> | supertest.SuperAgentTest} req
+ * @param {import("../app/db/schemas/users")} userObj 
+ * @returns
+ */
+const loginUser = async (req, userObj) => {
+    const res = await req
+        .post("/auth/login")
+        .set("Accept", "application/json")
+        .send(userObj);
+    return res;
+};
 
 beforeAll(async () => {
     process.env.NODE_ENV = "test";
@@ -82,85 +89,72 @@ afterAll(async () => {
 });
 
 describe("/auth", () => {
-    let refreshtoken;
+    const userData = {
+        email: "WeirdEmail@email.com",
+        firstname: "Harold",
+        lastname: "Jenkins"
+    };
+    let register;
+    let login;
+    let previousLogin;
 
     describe("POST /register", () => {
         it("should respond with status 400 when missing data", async () => {
-            const res = await supertest(testApp.app)
-                .post("/auth/register")
-                .set("Accept", "application/json")
-                .send({});
+            const res = await createUser({});
             expect(res.statusCode).toBe(400);
         });
         it("should respond with the user", async () => {
-            const res = await supertest(testApp.app)
-                .post("/auth/register")
-                .set("Accept", "application/json")
-                .send(sendData);
+            const res = await createUser({ ...userData, password: "password123" });
             expect(res.statusCode).toBe(201);
-            expect(res.body).toMatchObject(checkData);
-            user = res.body;
+            expect(res.body).toMatchObject(userData);
+            register = res.body;
         });
         it("should add the user to the database", async () => {
-            expect(await testApp.service.services.user.find({ email: sendData.email })).toMatchObject(checkData);
+            expect(await testApp.service.services.user.find({ email: userData.email })).toMatchObject(userData);
         });
         it("should remove key value pairs not on the schema", async () => {
-            const tempData = Object.assign({}, sendData);
-            delete tempData.email;
-            const res = await supertest(testApp.app)
-                .post("/auth/register")
-                .set("Accept", "application/json")
-                // Need a new email
-                .send({ ...tempData, email: "AnotherWeirdEmail@email.com", badKey: "badValue" });
+            const newUserData = {
+                email: "AnotherWeirdEmail@email.com",
+                firstname: "Leroy",
+                lastname: "Jenkins"
+            };
+            const res = await createUser({ ...newUserData, password: "password123", badKey: "badValue" });
             expect(res.statusCode).toBe(201);
-            expect(res.body).not.toMatchObject({ ...checkData, badKey: "badValue" });
+            expect(res.body).not.toMatchObject({ ...newUserData, password: "password123", badKey: "badValue" });
         });
     });
 
     describe("POST /login", () => {
         it("should respond with status 400 when missing data", async () => {
-            const res = await supertest(testApp.app)
-                .post("/auth/login")
-                .set("Accept", "application/json")
-                .send({});
+            const res = await loginUser(supertest(testApp.app), {});
             expect(res.statusCode).toBe(400);
         });
         it("should respond with status 404 with an invalid user", async () => {
-            const res = await supertest(testApp.app)
-                .post("/auth/login")
-                .set("Accept", "application/json")
-                .send({ email: "1", password: "1" });
+            const res = await loginUser(supertest(testApp.app), { email: "1", password: "1" });
             expect(res.statusCode).toBe(404);
         });
         it("should respond with status 401 when using incorrect password", async () => {
-            const res = await supertest(testApp.app)
-                .post("/auth/login")
-                .set("Accept", "application/json")
-                .send({ ...checkData, password: "wrongpassword123" });
+            const res = await loginUser(supertest(testApp.app), { ...userData, password: "wrongpassword123" });
             expect(res.statusCode).toBe(401);
         });
         it("should respond with correct data and cookies", async () => {
-            const res = await request
-                .post("/auth/login")
-                .set("Accept", "application/json")
-                .send(sendData);
+            const res = await loginUser(request, { ...userData, password: "password123" });
             expect(res.statusCode).toBe(200);
-            expect(res.body).toMatchObject({ userid: user._id });
+            expect(res.body).toMatchObject({ userid: register._id });
             expect(typeof res.body.expiresIn).toBe("string");
             expect(typeof res.body.token).toBe("string");
             expect(typeof res.body.refreshtoken).toBe("string");
             expect(res.headers["set-cookie"][0]).toContain("refresh_token=");
-            refreshtoken = res.body.refreshtoken;
-            jwttoken = res.body.token;
+            login = res.body;
+            previousLogin = Object.assign({}, login); // Save it twice (when login gets updated we still have previous)
         });
         it("should add the refreshtoken to the database", async () => {
-            expect(await testApp.service.services.auth.getRefreshToken(refreshtoken))
-                .toMatchObject({ _id: refreshtoken, userid: user._id });
+            expect(await testApp.service.services.auth.getRefreshToken(login.refreshtoken))
+                .toMatchObject({ _id: login.refreshtoken, userid: register._id });
         });
     });
 
     describe("POST /refresh_token", () => {
-        const previousToken = refreshtoken;
         it("should respond with 401 if an invalid token was provided", async () => {
             const res = await supertest(testApp.app)
                 .post("/auth/refresh_token")
@@ -173,20 +167,20 @@ describe("/auth", () => {
                 .set("Accept", "application/json")
                 .send();
             expect(res.statusCode).toBe(200);
-            expect(res.body).toMatchObject({ userid: user._id });
+            expect(res.body).toMatchObject({ userid: register._id });
             expect(typeof res.body.expiresIn).toBe("string");
             expect(typeof res.body.token).toBe("string");
             expect(typeof res.body.refreshtoken).toBe("string");
             expect(res.headers["set-cookie"][0]).toContain("refresh_token=");
-            refreshtoken = res.body.refreshtoken;
+            login = res.body;
         });
         it("should delete previous token", async () => {
-            expect(await testApp.service.services.auth.getRefreshToken(previousToken))
-                .not.toMatchObject({ _id: previousToken, userid: user._id });
+            expect(await testApp.service.services.auth.getRefreshToken(previousLogin.refreshtoken))
+                .not.toMatchObject({ _id: previousLogin.refreshtoken, userid: register._id });
         });
         it("should add the new token to the database", async () => {
-            expect(await testApp.service.services.auth.getRefreshToken(refreshtoken))
-                .toMatchObject({ _id: refreshtoken, userid: user._id });
+            expect(await testApp.service.services.auth.getRefreshToken(login.refreshtoken))
+                .toMatchObject({ _id: login.refreshtoken, userid: register._id });
         });
     });
 
@@ -204,32 +198,30 @@ describe("/auth", () => {
             expect(res.statusCode).toBe(200);
         });
         it("should delete the refresh_token", async () => {
-            expect(await testApp.service.services.auth.getRefreshToken(refreshtoken))
-                .not.toMatchObject({ _id: refreshtoken, userid: user._id });
+            expect(await testApp.service.services.auth.getRefreshToken(login.refreshtoken))
+                .not.toMatchObject({ _id: login.refreshtoken, userid: register._id });
         });
     });
 });
 
 describe("User routes", () => {
-    const newData = {
+    const usersData = [{
         email: "Batman@wayneind.com",
         firstname: "Bat",
         lastname: "man"
-    };
-    let newUser;
-    let newJwtToken;
+    }, {
+        email: "Superman@wayneind.com",
+        firstname: "Super",
+        lastname: "man"
+    }];
+    let registers = [];
+    let logins = [];
 
-    beforeAll(async () => { // Create a new user to edit and delete
-        const regRes = await supertest(testApp.app)
-            .post("/auth/register")
-            .set("Accept", "application/json")
-            .send({ ...newData, password: "password123" });
-        const logRes = await supertest(testApp.app)
-            .post("/auth/login")
-            .set("Accept", "application/json")
-            .send({ ...newData, password: "password123" });
-        newUser = regRes.body;
-        newJwtToken = logRes.body.token;
+    beforeAll(async () => { // Create new users for testing
+        registers[0] = (await createUser({ ...usersData[0], password: "iambatman" })).body;
+        registers[1] = (await createUser({ ...usersData[1], password: "superman123" })).body;
+        logins[0] = (await loginUser(supertest(testApp.app), { ...usersData[0], password: "iambatman" })).body;
+        logins[1] = (await loginUser(supertest(testApp.app), { ...usersData[1], password: "superman123" })).body;
     });
 
     it("should respond with 401 when an unauthorized user makes a request", async () => {
@@ -244,52 +236,51 @@ describe("User routes", () => {
         it("should respond with 404 if an invalid user was provided", async () => {
             const res = await supertest(testApp.app)
                 .get("/api/user/notauserid")
-                .set("authorization", `Bearer ${jwttoken}`)
+                .set("authorization", `Bearer ${logins[0].token}`)
                 .send();
             expect(res.statusCode).toBe(404);
         });
         it("should send back the user", async () => {
             const res = await supertest(testApp.app)
-                .get(`/api/user/${user._id}`)
-                .set("authorization", `Bearer ${jwttoken}`)
+                .get(`/api/user/${registers[0]._id}`)
+                .set("authorization", `Bearer ${logins[0].token}`)
                 .send();
             expect(res.statusCode).toBe(200);
-            expect(res.body).toMatchObject(checkData);
+            expect(res.body).toMatchObject(registers[0]);
         });
     });
 
     describe("PUT /:userid", () => {
+        const putData = {
+            email: "Notstrong@wayneind.com",
+            firstname: "Suck it",
+            lastname: "-BM"
+        };
+
         it("should respond with 404 if an invalid user was provided", async () => {
             const res = await supertest(testApp.app)
                 .put("/api/user/notauserid")
-                .set("authorization", `Bearer ${jwttoken}`)
+                .set("authorization", `Bearer ${logins[0].token}`)
                 .send();
             expect(res.statusCode).toBe(404);
         });
         it("should respond with 403 when trying to edit other users", async () => {
             const res = await supertest(testApp.app)
-                .put(`/api/user/${user._id}`)
-                .set("authorization", `Bearer ${newJwtToken}`)
+                .put(`/api/user/${registers[0]._id}`)
+                .set("authorization", `Bearer ${logins[1].token}`)
                 .send();
             expect(res.statusCode).toBe(403);
         });
         it("should send back the new user object", async () => {
             const res = await supertest(testApp.app)
-                .put(`/api/user/${user._id}`)
-                .set("authorization", `Bearer ${jwttoken}`)
-                .send(newData);
+                .put(`/api/user/${registers[1]._id}`)
+                .set("authorization", `Bearer ${logins[1].token}`)
+                .send(putData);
             expect(res.statusCode).toBe(200);
-            expect(res.body).toMatchObject(newData);
+            expect(res.body).toMatchObject(putData);
         });
         it("should update the database with the new data", async () => {
-            expect(await testApp.service.services.user.find({ _id: user._id })).toMatchObject(newData);
-        });
-
-        afterAll(async () => {
-            const res = await supertest(testApp.app)
-                .put(`/api/user/${user._id}`)
-                .set("authorization", `Bearer ${jwttoken}`)
-                .send(checkData); // Revert back to original data
+            expect(await testApp.service.services.user.find({ _id: registers[1]._id })).toMatchObject(putData);
         });
     });
 
@@ -297,32 +288,44 @@ describe("User routes", () => {
         it("should respond with 404 if an invalid user was provided", async () => {
             const res = await supertest(testApp.app)
                 .put("/api/user/notauserid")
-                .set("authorization", `Bearer ${jwttoken}`)
+                .set("authorization", `Bearer ${logins[0].token}`)
                 .send();
             expect(res.statusCode).toBe(404);
         });
         it("should respond with 403 when trying to delete other users", async () => {
             const res = await supertest(testApp.app)
-                .delete(`/api/user/${user._id}`)
-                .set("authorization", `Bearer ${newJwtToken}`)
+                .delete(`/api/user/${registers[0]._id}`)
+                .set("authorization", `Bearer ${logins[1].token}`)
                 .send();
             expect(res.statusCode).toBe(403);
         });
         it("should delete the user from the database", async () => {
             const res = await supertest(testApp.app)
-                .delete(`/api/user/${newUser._id}`)
-                .set("authorization", `Bearer ${newJwtToken}`)
+                .delete(`/api/user/${registers[0]._id}`)
+                .set("authorization", `Bearer ${logins[0].token}`)
                 .send();
             expect(res.statusCode).toBe(204);
-            expect(await testApp.service.services.user.find({ _id: newUser._id }))
-                .not.toMatchObject(newData);
+            expect(await testApp.service.services.user.find({ _id: registers[0]._id }))
+                .not.toMatchObject(registers[0]);
         });
     });
 });
 
 describe("Product routes", () => {
+    const userData = {
+        email: "Producttester@company.com",
+        firstname: "John",
+        lastname: "Doe"
+    };
+    let register;
+    let login; // New user for testing products
+
+
     let products = []; // Function to populate products
     beforeAll(async () => {
+        register = (await createUser({ ...userData, password: "mypassword" })).body;
+        login = (await loginUser(supertest(testApp.app), { ...userData, password: "mypassword" })).body;
+
         const tempProducts = seeds.products();
         for (const product of tempProducts) {
             products.push(await testApp.service.services.product.create(product));
@@ -333,7 +336,7 @@ describe("Product routes", () => {
         it("should return all products if no query is provided", async () => {
             const res = await supertest(testApp.app)
                 .get("/api/product")
-                .set("authorization", `Bearer ${jwttoken}`)
+                .set("authorization", `Bearer ${login.token}`)
                 .send();
             expect(res.statusCode).toBe(200);
             expect(res.body).toMatchObject(products);
@@ -342,7 +345,7 @@ describe("Product routes", () => {
             const matchProducts = products.filter(product => product.price === 15);
             const res = await supertest(testApp.app)
                 .get("/api/product")
-                .set("authorization", `Bearer ${jwttoken}`)
+                .set("authorization", `Bearer ${login.token}`)
                 .send({ price: 15 });
             expect(res.statusCode).toBe(200);
             expect(res.body.length).toBe(matchProducts.length);
