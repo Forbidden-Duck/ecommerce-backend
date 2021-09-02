969d36fc8466052b6876c288a125f601e604a2be
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-03 02:24:27 +1000

    production to run on HTTPS

--- app/index.js
+++ app/index.js
@@ -2,16 +2,28 @@
 require("dotenv").config({ path: __dirname + "/../process.env" });
 
 const app = require("express")();
+const http = require("http");
+const https = require("https");
 const loaders = require("./loaders");
 const { PORT } = require("../config");
 
 (async () => {
     await loaders(app);
-    app.listen(PORT, err => {
-        if (err) {
-            console.log("Error while listening for connections\n", err);
-            process.exit(10);
-        }
-        console.log(`Server listening on ${PORT}`);
-    });
+    if (process.env.NODE_ENV === "production") {
+        const fs = require("fs");
+        const httpsOptions = {
+            key: fs.readFileSync(__dirname + "/../cert/key.pem"),
+            cert: fs.readFileSync(__dirname + "/../cert/cert.pem"),
+        };
+        https.createServer(httpsOptions, app).listen(PORT);
+        console.log(`Server listening on https/${PORT}`);
+    } else {
+        app.listen(PORT, (err) => {
+            if (err) {
+                console.log("Error while listening for connections\n", err);
+                process.exit(10);
+            }
+            console.log(`Server listening on http/${PORT}`);
+        });
+    }
 })();
